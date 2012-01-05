f86474d21c3cbc830c6f1faf4deb8baab25b494b
Nicholas Ding <nicholasdsj@gmail.com>
2012-01-05 16:48:57 +0000

    Added barcode scanner

---
+++ barcode_scanner/B2SBarcodeScanner/B2SBarcodeScanner/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  B2SBarcodeScanner
+//
+//  Created by Ding Nicholas on 12/30/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ barcode_scanner/B2SBarcodeScanner/B2SBarcodeScanner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  B2SBarcodeScanner
+//
+//  Created by Ding Nicholas on 12/30/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
