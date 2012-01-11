34befaaee4b72d59619d930fbc7a685ca8792e22
Raphael Prevost <raphael@dragondollar.com>
2012-01-11 15:41:11 +0000

    * add a default currency to avoid foreign key constraint errors with Postgres

--- backtoshops/sales/migrations/0002_auto__add_productcurrency__add_field_product_currency.py
+++ backtoshops/sales/migrations/0002_auto__add_productcurrency__add_field_product_currency.py
@@ -3,6 +3,7 @@ import datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
+from sales.models import ProductCurrency
 
 class Migration(SchemaMigration):
 
@@ -16,7 +17,8 @@ class Migration(SchemaMigration):
             ('is_default', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
         db.send_create_signal('sales', ['ProductCurrency'])
-
+        eur = ProductCurrency(id=1, code='EUR',description='Euro',is_default=True)
+        eur.save()
         # Adding field 'Product.currency'
         db.add_column('sales_product', 'currency', self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['sales.ProductCurrency']), keep_default=False)
 
