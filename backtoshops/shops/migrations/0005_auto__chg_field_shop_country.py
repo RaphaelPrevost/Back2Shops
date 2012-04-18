7c33ae8d79291040d520a3cf8463a7d59ed7b405
Julian Ju <10335767+lakil00@users.noreply.github.com>
2012-04-18 12:58:16 +0000

    ticket #30 latest update. * now country is not a free text field. * when only one address is returned from Google map, page auto selects the country for users.
    - must run ./manage.py migrate countries
    - must run ./manage.py migrate shops

---
+++ backtoshops/shops/migrations/0005_auto__chg_field_shop_country.py
@@ -0,0 +1,65 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Renaming column for 'Shop.country' to match new field type.
+        db.rename_column('shops_shop', 'country', 'country_id')
+        # Changing field 'Shop.country'
+        db.alter_column('shops_shop', 'country_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['countries.Country'], null=True))
+
+        # Adding index on 'Shop', fields ['country']
+        db.create_index('shops_shop', ['country_id'])
+
+
+    def backwards(self, orm):
+        
+        # Removing index on 'Shop', fields ['country']
+        db.delete_index('shops_shop', ['country_id'])
+
+        # User chose to not deal with backwards NULL issues for 'Shop.country'
+        raise RuntimeError("Cannot reverse this migration. 'Shop.country' and its values cannot be restored.")
+
+
+    models = {
+        'accounts.brand': {
+            'Meta': {'object_name': 'Brand'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'countries.country': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'Country', 'db_table': "'country'"},
+            'iso': ('django.db.models.fields.CharField', [], {'max_length': '2', 'primary_key': 'True'}),
+            'iso3': ('django.db.models.fields.CharField', [], {'max_length': '3', 'null': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'numcode': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            'printable_name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        'shops.shop': {
+            'Meta': {'object_name': 'Shop'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'catcher': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['countries.Country']", 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'}),
+            'gestion_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {}),
+            'longitude': ('django.db.models.fields.FloatField', [], {}),
+            'mother_brand': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'shops'", 'to': "orm['accounts.Brand']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'opening_hours': ('django.db.models.fields.CharField', [], {'max_length': '1000', 'null': 'True', 'blank': 'True'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'upc': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'zipcode': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['shops']
