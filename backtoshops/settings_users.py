e5e348963c7a26217f83edb7e2bf8d31a6cc0947
Ada Ch?n <ada.chen.cs@gmail.com>
2013-09-12 02:59:51 +0000

    revert previous commits for #38: svn merge -r161:159 .

--- backtoshops/settings_users.py
+++
@@ -1,7 +0,0 @@
-# users.backtoshops.com settings
-
-import constants
-USER_CREATION_CAPTCHA = "1234" # temporary solution for now
-DEFAULT_HASH_ALGORITHM = 1 #whirlpool
-HASH_MIN_ITERATIONS = 2
-HASH_MAX_ITERATIONS = 256
