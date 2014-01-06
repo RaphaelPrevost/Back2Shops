83df6e599f2244d301c126e5ee6c0fb82a94ca5e
Jessica Qi <jessica@infinite-code.com>
2014-01-06 07:58:24 +0000

    refs #78, * revert settings: svn merge -r 233:232 settings.py

--- backtoshops/settings.py
+++ backtoshops/settings.py
@@ -9,7 +9,7 @@ elif node() == 'debian': #tentatively using this as VPS debian dev setting
 elif node() == 'Ghostwheel': #now i can run the code from my laptop!
     from settings_ghostwheel import *
 elif node(): # == PRODUCTION_HOST:
-    from settings_local import *
+    from settings_production import *
 else:
     raise Exception("Cannot determine execution mode for host '%s'.  Please check DEVELOPMENT_HOST and PRODUCTION_HOST in settings_local.py." % node())
 
