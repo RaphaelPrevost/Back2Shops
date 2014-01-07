91812f08745fa27caceaa6da3754ef718bdb446b
Jessica Qi <jessica@infinite-code.com>
2014-01-07 06:36:07 +0000

    CRF #61, * make carriers with api read-only on UI. * fixture file is an example to add carriers with api, need update when supported carriers with API fixed.

---
+++ backtoshops/shippings/migrations/0005_add_carrier_services_data.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+from django.core.management import call_command
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        call_command("loaddata", "shippings/fixtures/carriers_with_api.json")
+
+    def backwards(self, orm):
+        pass
+
+    models = {
+        u'accounts.brand': {
+            'Meta': {'object_name': 'Brand'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'shippings.carrier': {
+            'Meta': {'object_name': 'Carrier'},
+            'flag': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'shippings.customshippingrate': {
+            'Meta': {'object_name': 'CustomShippingRate'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'seller': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['accounts.Brand']"}),
+            'shipment_type': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'shipping_rate': ('django.db.models.fields.FloatField', [], {}),
+            'total_order_lower': ('django.db.models.fields.FloatField', [], {}),
+            'total_order_type': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'total_order_upper': ('django.db.models.fields.FloatField', [], {})
+        },
+        u'shippings.customshippingrateinshipping': {
+            'Meta': {'object_name': 'CustomShippingRateInShipping'},
+            'custom_shipping_rate': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['shippings.CustomShippingRate']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'shipping': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['shippings.Shipping']"})
+        },
+        u'shippings.flatrateinshipping': {
+            'Meta': {'object_name': 'FlatRateInShipping'},
+            'flat_rate': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'shipping': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['shippings.Shipping']"})
+        },
+        u'shippings.service': {
+            'Meta': {'object_name': 'Service'},
+            'carrier': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'services'", 'to': u"orm['shippings.Carrier']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'shippings.serviceinshipping': {
+            'Meta': {'object_name': 'ServiceInShipping'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'service': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['shippings.Service']"}),
+            'shipping': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['shippings.Shipping']"})
+        },
+        u'shippings.shipping': {
+            'Meta': {'object_name': 'Shipping'},
+            'allow_group_shipment': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_pickup': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'handling_fee': ('django.db.models.fields.FloatField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pickup_voids_handling_fee': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'shipping_calculation': ('django.db.models.fields.SmallIntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['shippings']
