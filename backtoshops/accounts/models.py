aa329364b138787fb08cd5a99f4c6025d5a52196
Jessica Qi <jessica@infinite-code.com>
2014-02-21 02:23:56 +0000

    refs #91 - Create a shipping fees calculation API(user backtoshops-account part), * implementation in user server:    * /webservice/1.0/pub/shipping/list    * /webservice/1.0/pub/shipping/fees    * /webservice/1.0/pub/shipping/conf    * dd tests for shipping/list, shipping fees, shipping conf    * some bug fixes found when add test * implementation in backoffice server:    * make address into one table for shop's address and corporate account address * TODO:    * update wiki page

--- backtoshops/accounts/models.py
+++ backtoshops/accounts/models.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+from address.models import Address
 
-from countries.models import Country
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
@@ -17,16 +17,8 @@ class UserProfile(models.Model):
 class Brand(models.Model):
     name = models.CharField(max_length=50)
     logo = models.ImageField(upload_to="brand_logos")
-    address = models.CharField(verbose_name=_("Address"), max_length=250, blank=True, null=True)
-    zipcode = models.IntegerField(verbose_name=_("Postal code"), blank=True, null=True)
-    city = models.CharField(verbose_name=_("City"), max_length=100)
-    country = models.ForeignKey(Country, verbose_name=_('Country'), blank=True, null=True)
-    province_code = models.CharField(verbose_name=_('Province'), max_length=2, blank=True, null=True)
+    address = models.ForeignKey(Address, unique=True)
 
     def __str__(self):
         return self.name
-
-    def clean_city(self):
-        return self.city.upper()
-
 
