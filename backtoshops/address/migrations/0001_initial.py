a1968d6a4da6f666dfe2f507c771b8c26a2adc95
Jessica Qi <jessica@infinite-code.com>
2014-02-21 02:25:14 +0000

    refs #91 - Create a shipping fees calculation API(backtoshops-address part), * implementation in user server:    * /webservice/1.0/pub/shipping/list    * /webservice/1.0/pub/shipping/fees    * /webservice/1.0/pub/shipping/conf    * dd tests for shipping/list, shipping fees, shipping conf    * some bug fixes found when add test * implementation in backoffice server:    * make address into one table for shop's address and corporate account address * TODO:    * update wiki page

---
+++ backtoshops/address/migrations/0001_initial.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+    depends_on = ( ("countries", "0001_initial"), )
+
+    def forwards(self, orm):
+        # Adding model 'Address'
+        db.create_table(u'address_address', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('address', self.gf('django.db.models.fields.CharField')(max_length=250, null=True, blank=True)),
+            ('zipcode', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
+            ('city', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('country', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['countries.Country'], null=True, blank=True)),
+            ('province_code', self.gf('django.db.models.fields.CharField')(max_length=2, null=True, blank=True)),
+        ))
+        db.send_create_signal(u'address', ['Address'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Address'
+        db.delete_table(u'address_address')
+
+
+    models = {
+        u'address.address': {
+            'Meta': {'object_name': 'Address'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['countries.Country']", 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'province_code': ('django.db.models.fields.CharField', [], {'max_length': '2', 'null': 'True', 'blank': 'True'}),
+            'zipcode': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'countries.country': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'Country', 'db_table': "'country'"},
+            'iso': ('django.db.models.fields.CharField', [], {'max_length': '2', 'primary_key': 'True'}),
+            'iso3': ('django.db.models.fields.CharField', [], {'max_length': '3', 'null': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'numcode': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            'printable_name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        }
+    }
+
+    complete_apps = ['address']
