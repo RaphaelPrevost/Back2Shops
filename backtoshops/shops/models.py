cad756bfee3a40a39d6009d1cb2938caa823684f
Jessica Qi <jessica@infinite-code.com>
2014-02-21 04:05:50 +0000

    refs #91 - Create a shipping fees calculation API(backtoshops-shops part), * implementation in user server:    * /webservice/1.0/pub/shipping/list    * /webservice/1.0/pub/shipping/fees    * /webservice/1.0/pub/shipping/conf    * dd tests for shipping/list, shipping fees, shipping conf    * some bug fixes found when add test * implementation in backoffice server:    * make address into one table for shop's address and corporate account address * TODO:    * update wiki page

--- backtoshops/shops/models.py
+++ backtoshops/shops/models.py
@@ -6,9 +6,9 @@ from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
 
 from accounts.models import Brand
+from address.models import Address
 from common.cache_invalidation import post_delete_handler
 from common.cache_invalidation import post_save_handler
-from countries.models import Country
 from shippings.models import Shipping
 from sorl.thumbnail import ImageField
 
@@ -17,11 +17,6 @@ class Shop(models.Model):
     mother_brand = models.ForeignKey(Brand, related_name="shops")
     gestion_name = models.CharField(verbose_name=_("Internal name"), max_length=100, blank=True, null=True)
     upc = models.CharField(verbose_name=_("Barcode"), max_length=50, blank=True, null=True)
-    address = models.CharField(verbose_name=_("Address"), max_length=250, blank=True, null=True)
-    zipcode = models.IntegerField(verbose_name=_("Postal code"), blank=True, null=True)
-    city = models.CharField(verbose_name=_("City"), max_length=100)
-    country = models.ForeignKey(Country, verbose_name=_('Country'), blank=True, null=True)
-    province_code = models.CharField(verbose_name=_('Province'), max_length=2, blank=True, null=True)
     phone = models.CharField(verbose_name=_("Phone number"), max_length=50, blank=True, null=True)
     name = models.CharField(verbose_name=_("Shop name"), max_length=50)
     catcher = models.CharField(verbose_name=_("Caption"), max_length=250, blank=True, null=True)
@@ -30,13 +25,8 @@ class Shop(models.Model):
     opening_hours = models.CharField(max_length=1000, blank=True, null=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    address = models.ForeignKey(Address, unique=True)
 
-    def clean_city(self):
-        return self.city.upper()
-
-    def save(self, *args, **kwargs):
-        self.city = self.city.upper()
-        super(Shop, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.name
