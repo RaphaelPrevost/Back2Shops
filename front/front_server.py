9f55496933d9c2750380b48ca5d76c2971d9bdb3
Ada Ch?n <ada.chen.cs@gmail.com>
2014-04-28 09:31:22 +0000

    refs #120, serving static files

--- front/front_server.py
+++ front/front_server.py
@@ -7,6 +7,7 @@ from gevent.pywsgi import WSGIServer
 
 import settings
 from common.data_access import data_access
+import static
 from urls import urlpatterns
 from B2SUtils.log import setupLogging
 from B2SFrontUtils.constants import REMOTE_API_NAME
@@ -21,6 +22,11 @@ app = api = falcon.API(before=[parse_form_params])
 # Resources are represented by long-lived class instances
 for url, res in urlpatterns.iteritems():
     api.add_route(url, res())
+# serve static files
+static_path = settings.STATIC_FILES_PATH
+api.add_route('/js/{name}', static.JsItem(static_path + '/js/'))
+api.add_route('/css/{name}', static.CssItem(static_path + '/css/'))
+api.add_route('/img/{name}', static.ImgItem(static_path + '/img/'))
 
 
 def load_data():
