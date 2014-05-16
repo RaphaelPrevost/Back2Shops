302b90925aa252b01ca25921f662233a302014aa
Ada Ch?n <ada.chen.cs@gmail.com>
2014-05-16 09:46:23 +0000

    refs #131, set assets upload directory in production

--- assets/settings_product.py
+++ assets/settings_product.py
@@ -3,7 +3,7 @@ from settings_local import *
 
 PRODUCTION = True
 
-STATIC_FILES_PATH = '/home/backtoshops/assets/static'
+STATIC_FILES_PATH = '/home/backtoshops/assets_files'
 ADM_ROOT_URI = "http://37.187.48.33"
 SERVER_APIKEY_URI_MAP = {
     'ADM': os.path.join(ADM_ROOT_URI, 'webservice/1.0/pub/apikey.pem'),
