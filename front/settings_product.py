edd8c0030c07dcbb5aa57a3b7a0a24e15ab10e69
Ada Ch?n <ada.chen.cs@gmail.com>
2014-05-26 09:34:10 +0000

    refs #127, fix settings and add comments

--- front/settings_product.py
+++ front/settings_product.py
@@ -4,10 +4,14 @@ from settings_local import *
 PRODUCTION = True
 
 USR_ROOT_URI = "http://92.222.30.2"
+SERVER_APIKEY_URI_MAP = {
+    'USR': os.path.join(USR_ROOT_URI,
+                        'webservice/1.0/pub/apikey.pem'),
+}
 USER_PM_FORM_URL = '%s/webservice/1.0/pub/payment/form' % USR_ROOT_URI
 
 FRONT_ROOT_URI = "http://92.222.30.5"
 PP_SUCCESS = "%s/paypal/%%(id_trans)s/success" % FRONT_ROOT_URI
 PP_FAILURE = "%s/paypal/%%(id_trans)s/failure" % FRONT_ROOT_URI
 
-BRAND_ID = "1000001" #TODO
+BRAND_ID = "1"
