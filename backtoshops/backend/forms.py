8f83b1d8869cea1e9af1ee73655bc5ae4afdba62
Doris Tang <doris@infinite-code.com>
2014-06-11 09:41:46 +0000

    * Refs #146, make the thumbnail and picture of category to be not required.

--- backtoshops/backend/forms.py
+++ backtoshops/backend/forms.py
@@ -169,8 +169,8 @@ class SAUserForm(BaseUserForm):
 class SACategoryForm(forms.ModelForm):
     error_css_class = 'error'
     required_css_class = 'required'
-    thumbnail = forms.ImageField(widget=AdvancedFileInput)
-    picture = forms.ImageField(widget=AdvancedFileInput)
+    thumbnail = forms.ImageField(widget=AdvancedFileInput, required=False)
+    picture = forms.ImageField(widget=AdvancedFileInput, required=False)
 
     def __init__(self, *args, **kwargs):
         super(SACategoryForm, self).__init__(*args, **kwargs)
