d621069bad71b3e890cc60a9dba07d1ee203152d
Raphael Prevost <raphael@dragondollar.com>
2014-07-18 06:58:54 +0000

    * commit the diff on pre_prod FO .

--- front/settings.py
+++ front/settings.py
@@ -1,3 +1,18 @@
 # frontend settings
 from settings_local import *
 
+PRODUCTION = True
+
+TEMPLATE_PATH = ['views/templates/breuer', 'views/templates/dragondollar']
+
+USR_ROOT_URI = "http://92.222.30.2"
+SERVER_APIKEY_URI_MAP = {
+    'USR': os.path.join(USR_ROOT_URI,
+                        'webservice/1.0/pub/apikey.pem'),
+}
+
+FRONT_ROOT_URI = "http://92.222.30.5"
+PP_SUCCESS = "%s/paypal/%%(id_trans)s/success" % FRONT_ROOT_URI
+PP_FAILURE = "%s/paypal/%%(id_trans)s/failure" % FRONT_ROOT_URI
+
+BRAND_ID = "1"
