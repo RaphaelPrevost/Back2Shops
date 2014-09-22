57db9b15680d34da00f13b02ebfc09a4d8e31e34
Raphael Prevost <raphael@dragondollar.com>
2014-09-22 06:37:34 +0000

    * extract servers' address from deploy.sh to *_settings.sh

---
+++ breuer_settings.sh
@@ -0,0 +1,13 @@
+export PRODUCTION='true'
+
+export ADM_DOMAIN=${ADM_DOMAIN:-sales.backtoshops.com}
+export USR_DOMAIN=${USR_DOMAIN:-usr.backtoshops.com}
+export FIN_DOMAIN=${FIN_DOMAIN:-finance.backtoshops.com}
+export AST_DOMAIN=${AST_DOMAIN:-assets.backtoshops.com}
+export FRT_DOMAIN=${FRT_DOMAIN:-front.backtoshops.com}
+
+export ADM_ADDR='37.187.48.33:80'
+export USR_ADDR='92.222.30.2:80'
+export FIN_ADDR='92.222.30.3:80'
+export AST_ADDR='92.222.30.4:80'
+export FRT_ADDR='92.222.30.5:80'
---
+++ testing_settings.sh
@@ -0,0 +1,13 @@
+export PRODUCTION='false'
+
+export ADM_DOMAIN=${ADM_DOMAIN:-sales.backtoshops.com}
+export USR_DOMAIN=${USR_DOMAIN:-usr.backtoshops.com}
+export FIN_DOMAIN=${FIN_DOMAIN:-finance.backtoshops.com}
+export AST_DOMAIN=${AST_DOMAIN:-assets.backtoshops.com}
+export FRT_DOMAIN=${FRT_DOMAIN:-front.backtoshops.com}
+
+export ADM_ADDR='188.165.192.57:80'
+export USR_ADDR='188.165.192.57:18100'
+export FIN_ADDR='188.165.192.57:19000'
+export AST_ADDR='188.165.192.57:19300'
+export FRT_ADDR='188.165.192.57:19500'
