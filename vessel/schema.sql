0abd58fe8fe1e08a7b37deba511f0244414c2b02
Raphael Prevost <raphael@dragondollar.com>
2014-11-13 10:12:48 +0000

    * vessel: bugfix, add index

--- vessel/schema.sql
+++ vessel/schema.sql
@@ -9,6 +9,11 @@ CREATE TABLE vessel (
     photos text,
     next_update_time timestamp without time zone DEFAULT now() NOT NULL
 );
+CREATE INDEX vessel_by_name ON vessel USING btree (name);
+CREATE INDEX vessel_by_imo ON vessel USING btree (imo);
+CREATE INDEX vessel_by_mmsi ON vessel USING btree (mmsi);
+CREATE INDEX vessel_by_next_update_time ON vessel USING btree (next_update_time);
+
 
 CREATE TABLE vessel_navigation (
     id serial PRIMARY KEY,
@@ -21,6 +26,9 @@ CREATE TABLE vessel_navigation (
     arrival_time timestamp without time zone NOT NULL,
     created timestamp without time zone DEFAULT now() NOT NULL
 );
+CREATE INDEX vessel_navigation_by_created ON vessel_navigation USING btree (created desc);
+CREATE INDEX vessel_navigation_by_arrival_time ON vessel_navigation USING btree (arrival_time desc);
+
 
 CREATE TABLE vessel_position (
     id serial PRIMARY KEY,
@@ -33,6 +41,8 @@ CREATE TABLE vessel_position (
     status character varying(64),
     time timestamp without time zone DEFAULT now() NOT NULL
 );
+CREATE INDEX vessel_position_by_time ON vessel_position USING btree (time desc);
+
 
 CREATE TABLE country (
     iso character varying(2) PRIMARY KEY,
@@ -42,12 +52,15 @@ CREATE TABLE country (
     numcode smallint
 );
 
+
 CREATE TABLE user_fleet (
     id serial PRIMARY KEY,
     id_user BIGINT NOT NULL,
     imo character varying(64),
     mmsi character varying(64)
 );
+CREATE INDEX user_fleet_by_id_user ON user_fleet USING btree (id_user);
+
 
 CREATE TABLE vessel_arrival_notif (
     id serial PRIMARY KEY,
@@ -57,6 +70,9 @@ CREATE TABLE vessel_arrival_notif (
     mmsi character varying(64),
     done boolean DEFAULT false
 );
+CREATE INDEX vessel_arrival_notif_by_id_user_not_done ON vessel_arrival_notif USING btree (id_user) WHERE not done;
+CREATE INDEX vessel_arrival_notif_by_mmsi_not_done ON vessel_arrival_notif USING btree (mmsi) WHERE not done;
+
 
 CREATE TABLE container_arrival_notif (
     id serial PRIMARY KEY,
@@ -65,3 +81,7 @@ CREATE TABLE container_arrival_notif (
     container character varying(64),
     done boolean DEFAULT false
 );
+CREATE INDEX container_arrival_notif_by_id_user_not_done ON container_arrival_notif USING btree (id_user) WHERE not done;
+CREATE INDEX container_arrival_notif_by_container_not_done ON container_arrival_notif USING btree (container) WHERE not done;
+
+
