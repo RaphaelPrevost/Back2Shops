0c462e9b78d0834e1afe4dc55bd83d1ba41c7950
Jessica Qi <jessica@infinite-code.com>
2014-11-14 06:00:52 +0000

    refs #179, * revert configs deleted when do update

--- front/settings_product_vessel.py
+++ front/settings_product_vessel.py
@@ -1,5 +1,6 @@
 from settings_product import *
 
 TEMPLATE_PATH = ['views/templates/vessel', 'views/templates/breuer']
+TIMEZONE = 'Hongkong'
 
 STATIC_FILES_PATH = '/var/local/assets/vesselfront_files'
