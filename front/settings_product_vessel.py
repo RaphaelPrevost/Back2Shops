271563edbed4b075136d69331cd0d6d12c9e13ce
Jessica Qi <jessica@infinite-code.com>
2014-11-14 03:39:20 +0000

    refs #179, * Protect asset server data when deploying * strongest code when thumbnail pictures not exist

--- front/settings_product_vessel.py
+++ front/settings_product_vessel.py
@@ -1,4 +1,5 @@
 from settings_product import *
 
 TEMPLATE_PATH = ['views/templates/vessel', 'views/templates/breuer']
-TIMEZONE = 'Hongkong'
+
+STATIC_FILES_PATH = '/var/local/assets/vesselfront_files'
