15fb229336f7d2a86595df3032ab732f6fb1686b
Raphaël Prevost <raph@el.bzh>
2018-08-28 07:39:23 -0400

    Fix issue with timestamps in User server transactions

--- users/models/transaction.py
+++ users/models/transaction.py
@@ -49,10 +49,17 @@ from B2SProtocol.constants import TRANS_STATUS
 def create_trans(conn, id_order, id_invoices,
                  amount_due, cookie,
                  status=TRANS_STATUS.TRANS_OPEN,
-                 create_time=datetime.now(),
-                 update_time=datetime.now()):
+                 create_time=None,
+                 update_time=None):
+    
     if isinstance(id_invoices, list):
         id_invoices = ujson.dumps(id_invoices)
+    
+    if (create_time == None):
+        create_time = datetime.now()
+    if (update_time == None):
+        update_time = datetime.now()
+    
     values = {
         'id_order': id_order,
         'id_invoices': id_invoices,
